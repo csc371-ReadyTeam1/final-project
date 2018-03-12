9175f47d91eee01a23f6c0ae35cb7b904dccec1f
Scott Kauker <swkauker@yahoo.com>
2018-03-11 17:18:29 -0700

    Fixed parallax not looping seamlessly

--- Assets/Scripts/CameraParallax.cs
+++ Assets/Scripts/CameraParallax.cs
@@ -13,24 +13,35 @@ public class CameraParallax : MonoBehaviour {
         int i = 0;
         foreach (Transform child in transform)
         {
-            layers[i] = child.GetComponent<SpriteRenderer>();
+            SpriteRenderer render = child.GetComponent<SpriteRenderer>();
+
+            //Ensure sprite widths are exactly twice their source size
+            render.size = new Vector2(render.sprite.bounds.size.x * 2, render.size.y);
+
+            layers[i] = render;
             i++;
         }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        int i = 1;
-		foreach (SpriteRenderer sprite in layers)
-        {
-            float delta = Camera.main.transform.position.x * 0.5f * ( i * 0.4f);
-            float offset = delta % (sprite.size.x * 0.1f);
 
-            //this is totally wrong but it doesn't really matter
-            sprite.gameObject.transform.position = new Vector2(Camera.main.transform.position.x - offset + sprite.size.x * 0.1f * 0.5f, transform.position.y);
+    // Update is called once per frame
+    void Update()
+    {
+        int i = 1;
+        foreach (SpriteRenderer sprite in layers)
+        {
+            float spriteWidth = sprite.transform.localScale.x * sprite.size.x;
+
+            //Determines the 'virtual' movement of this layer
+            float offset = Camera.main.transform.position.x * 0.5f * i * 0.4f;
+
+            //Loop back on itself at half the sprite's width, around the center
+            offset = offset % (spriteWidth * 0.5f);
+            offset -= spriteWidth * 0.25f;
+
+            //Set the actual position of the sprite
+            sprite.gameObject.transform.position = new Vector2(Camera.main.transform.position.x - offset, transform.position.y);
 
             i++;
         }
-
     }
 }
